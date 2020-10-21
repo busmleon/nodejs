74bf1f41ed040f4feda672321eff7eb8d69e8f50
busmleon <leon.schoolmail@web.de>
2020-10-21 14:20:09 +0200

    prod

--- config/keycloak-config.js
+++ config/keycloak-config.js
@@ -4,11 +4,15 @@ var Keycloak = require('keycloak-connect');
 let _keycloak;
 
 var keycloakConfig = {
+  // clientId: 'NodeJS',
   clientId: process.env.KEYCLOAK_BACKEND_CLIENT,
   bearerOnly: true,
+  // serverUrl: 'http://localhost:8080/auth',
   serverUrl: process.env.KEYCLOAK_URL,
+  // realm: 'APP_REALM',
   realm: process.env.KEYCLOAK_REALM,
   credentials: {
+    // secret: '750bf571-c29c-417e-9757-0d1f4d11e37e'
     secret: process.env.KEYCLOAK_BACKEND_CLIENT_SECRET
   }
 };
--- controller/db-controller.js
+++ controller/db-controller.js
@@ -1,13 +1,32 @@
 var express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 var router = express.Router();
 const keycloak = require('../config/keycloak-config.js').getKeycloak();
 
+// const uri = 'mongodb://root:root@localhost';
+const uri = process.env.MONGO_CONNECTION_STRING;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+var collection;
+
+client.connect().then(() => {
+  collection = client.db(process.env.MONGO_INITDB_DATABASE).collection('users');
+  console.log('Connected to DB');
+}).catch(err => {
+  res.json({ message: err });
+});
+
 router.get('/user', keycloak.protect(['user', 'admin']), function (req, res) {
-  res.send("Hello User");
+  try {
+    collection.find().toArray().then(users => {
+      res.send(users);
+    })
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 router.get('/admin', keycloak.protect('admin'), function (req, res) {
   res.send("Hello Admin");
+  //TODO: admin only access
 });
-
 module.exports = router;
