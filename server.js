c159a9b7d8a40adf275c71d27786b0f7fba60c37
busmleon <leon.schoolmail@web.de>
2020-10-22 10:01:25 +0200

    defaul nodejs

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-var corsOption = {
-  origin: process.env.FRONTEND_URL,
-  optionsSuccessStatus: 200
-}
-app.use(cors(corsOption));
-const keycloak = require('./config/keycloak-config.js').initKeycloak();
-app.use(keycloak.middleware());
+// const cors = require('cors');
+// var corsOption = {
+//   origin: process.env.FRONTEND_URL,
+//   optionsSuccessStatus: 200
+// }
+// app.use(cors(corsOption));
+// const keycloak = require('./config/keycloak-config.js').initKeycloak();
+// app.use(keycloak.middleware());
 
-app.use(require('body-parser').json());
+// app.use(require('body-parser').json());
 
-var dbController = require('./controller/db-controller.js');
-app.use('/db', dbController);
+// var dbController = require('./controller/db-controller.js');
+// app.use('/db', dbController);
 
 app.get('/', function (req, res) {
+  res.send('test');
   res.sendStatus(200);
 });
 
-app.listen(8000, 'backend.busmleon.de');
+app.listen(8000);
