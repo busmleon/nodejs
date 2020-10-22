52eaa57c350767a74e5ff54f3480b29c2362584b
busmleon <leon.schoolmail@web.de>
2020-10-22 11:55:25 +0200

    admins

--- controller/db-controller.js
+++ controller/db-controller.js
@@ -6,10 +6,11 @@ const keycloak = require('../config/keycloak-config.js').getKeycloak();
 // const uri = 'mongodb://root:root@localhost';
 const uri = process.env.MONGO_CONNECTION_STRING;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+var database;
 var collection;
 
 client.connect().then(() => {
-  collection = client.db(process.env.MONGO_INITDB_DATABASE).collection('users');
+  database = client.db(process.env.MONGO_INITDB_DATABASE);
   console.log('Connected to DB');
 }).catch(_ => {
   console.log('Connection to DB failed!');
@@ -17,6 +18,7 @@ client.connect().then(() => {
 
 router.get('/user', keycloak.protect(['user', 'admin']), function (req, res) {
   try {
+    collection = database.collection('users');
     collection.find().toArray().then(users => {
       res.send(users);
     })
@@ -26,7 +28,13 @@ router.get('/user', keycloak.protect(['user', 'admin']), function (req, res) {
 });
 
 router.get('/admin', keycloak.protect('admin'), function (req, res) {
-  res.send("Hello Admin");
-  //TODO: admin only access
+  try {
+    collection = database.collection('admins');
+    collection.find().toArray().then(admins => {
+      res.send(users);
+    })
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 module.exports = router;
