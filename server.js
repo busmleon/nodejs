8b5145b9a94d60d47e25ec4b6f414c643ffa0eac
busmleon <leon.schoolmail@web.de>
2021-03-30 16:38:22 +0200

    anged back

--- server.js
+++ server.js
@@ -2,27 +2,25 @@ const Express = require('express')
 const app = Express()
 const cors = require('cors')
 
-// const corsOption = {
-//   origin: process.env.FRONTEND_URL,
-//   optionsSuccessStatus: 200
-// }
+const corsOption = {
+  origin: process.env.FRONTEND_URL,
+  optionsSuccessStatus: 200
+}
 
-// app.use(cors(corsOption))
+app.use(cors(corsOption))
 
 app.get('/', (_req, res) => {
-  // res.send('Backend is running!')
-  res.sendStatus(200)
   // res.send('Backend is running! URL to Frontend: ' + process.env.FRONTEND_URL)
-  // res.redirect(200, process.env.FRONTEND_URL)
+  res.redirect(200, process.env.FRONTEND_URL)
   // res.redirect(process.env.FRONTEND_URL)
 })
 
-// const keycloak = require('./config/keycloak-config.js').initKeycloak()
-// app.use(keycloak.middleware())
+const keycloak = require('./config/keycloak-config.js').initKeycloak()
+app.use(keycloak.middleware())
 
-// app.use(require('body-parser').json())
+app.use(require('body-parser').json())
 
-// const dbController = require('./controller/db-controller.js')
-// app.use('/db', dbController)
+const dbController = require('./controller/db-controller.js')
+app.use('/db', dbController)
 
 app.listen(3000)
